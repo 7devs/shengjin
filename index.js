34bd20f4ace6082e86af53c12683e1ddb73da9ef
sheng jin <imshengjin@163.com>
2016-09-29 19:44:27 +0800

    //

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')(),
     bodyParser = require('body-parser');
-    wechat = require('./lib/wechat.js');
-    conf = require('./lib/config.js')
+    wechat = require('./lib/wechat');
+    conf = require('./lib/config')
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -10,5 +10,5 @@ wechat(conf.wechat);
 wechat.createMenu(require('./lib/menu.json'));
 app.use('/wxapi',require('./lib/routers/wxapi.js'));
 app.listen(8001,function(err){
-    console.log('listenning at 8001');
+    console.log('listenning at 8001...');
 })
